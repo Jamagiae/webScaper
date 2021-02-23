58674b6894bdec5eab539598de884afde66e5a3c
Jamagiae Medley <jamagiae.medley@outlook.com>
2021-02-23 14:07:05 -0500

    Add project files.

---
+++ webScapper/webScapper.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('https://forecast.weather.gov/MapClick.php?lat=44.64196&lon=-124.04110#.YDUib-hKiUk')
+soup = BeautifulSoup(page.content, 'html.parser')
+week = soup.find(id="seven-day-forecast-body")
+#print(week)
+
+items = week.find_all(class_='tombstone-container')
+#print(items[0])
+
+print(items[0].find(class_='period-name').get_text())
+print(items[0].find(class_='short-desc').get_text())
+print(items[0].find(class_='temp').get_text())
+
+period_name = [item.find(class_='period-name').get_text() for item in items]
+short_description = [item.find(class_='short-desc').get_text() for item in items]
+temperatures = [item.find(class_='temp').get_text() for item in items]
+
+
+weather_stuff = pd.DataFrame(
+    {
+        'period': period_name,
+        'short_description': short_description,
+        'temperatures': temperatures,
+    })
+
+
+print(weather_stuff)
+
+weather_stuff.to_csv('webscrapper.csv')
